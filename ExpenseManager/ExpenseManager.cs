c6dfcd67c51d472b0aa52d9be89346598ac2f16c
majidpawar9 <majidpawar9@gmail.com>
2023-11-28 17:45:50 -0500

    init

---
+++ ExpenseManager/ExpenseManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Expense.Services
+{
+    public class ExpenseManager
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
