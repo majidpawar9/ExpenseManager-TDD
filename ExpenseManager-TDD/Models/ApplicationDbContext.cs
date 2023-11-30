3f6d70074cdc94fa939fe8d79ea7233adb7520cb
mahek <mahektailor28@gmail.com>
2023-11-30 15:41:44 -0500

    Added Migration and Application DbContext

--- ExpenseManager-TDD/Models/ApplicationDbContext.cs
+++ ExpenseManager-TDD/Models/ApplicationDbContext.cs
@@ -4,10 +4,10 @@ namespace ExpenseManager_TDD.Models;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions options):base(Options){
+    public ApplicationDbContext(DbContextOptions options):base(options){
 
     }
 
     public DbSet<Transaction> Transactions {get; set;}
-    public Dbset<Category> Categories {get; set;}
+    public DbSet<Category> Categories {get; set;}
 }
