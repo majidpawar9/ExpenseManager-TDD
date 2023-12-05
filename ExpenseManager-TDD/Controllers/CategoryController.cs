8bd76ddc78de28e2498869f678b17d3793c979de
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-05 17:21:42 -0500

    Merged Add and Edit Functions in Controller

--- ExpenseManager-TDD/Controllers/CategoryController.cs
+++ ExpenseManager-TDD/Controllers/CategoryController.cs
@@ -27,15 +27,15 @@ namespace ExpenseManager_TDD.Controllers
         }
 
         // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? ID)
         {
-            if (id == null || _context.Categories == null)
+            if (ID == null || _context.Categories == null)
             {
                 return NotFound();
             }
 
             var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryID == id);
+                .FirstOrDefaultAsync(m => m.CategoryID == ID);
             if (category == null)
             {
                 return NotFound();
@@ -44,79 +44,43 @@ namespace ExpenseManager_TDD.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
-        public IActionResult Create()
+        // GET: Category/AddorEdit
+        public IActionResult AddOrEdit(int id = 0)
         {
-            return View(new Category());
+            if(id == 0)
+            {
+                return View(new Category());
+            }
+            else
+            {
+                return (View(_context.Categories.Find(id)));
+            }
         }
 
-        // POST: Category/Create
+        // POST: Category/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryID,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryID,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                if(category.CategoryID == 0) {
+                    _context.Add(category);
+                }
+                else
+                {
+                    _context.Update(category);
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
         }
 
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
 
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryID,Title,Icon,Type")] Category category)
-        {
-            if (id != category.CategoryID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.CategoryID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
+       
         // GET: Category/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
