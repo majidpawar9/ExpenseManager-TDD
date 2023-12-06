aa084dc11c0f4eb06128221d2fc43ae541f38607
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-06 16:46:08 -0500

    Connected all API to Main Page .

--- ExpenseManager-TDD/Controllers/CategoryController.cs
+++ ExpenseManager-TDD/Controllers/CategoryController.cs
@@ -26,23 +26,6 @@ namespace ExpenseManager_TDD.Controllers
                           Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? ID)
-        {
-            if (ID == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryID == ID);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/AddorEdit
         public IActionResult AddOrEdit(int id = 0)
@@ -102,9 +85,6 @@ namespace ExpenseManager_TDD.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryID == id)).GetValueOrDefault();
-        }
+       
     }
 }
