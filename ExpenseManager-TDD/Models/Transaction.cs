8bb41cd3143de2bf2d0c15ef6d432181f82984b8
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-08 18:46:06 -0500

    Added Transaction Controller  and its function

--- ExpenseManager-TDD/Models/Transaction.cs
+++ ExpenseManager-TDD/Models/Transaction.cs
@@ -9,10 +9,28 @@ public class Transaction
     public int TransactionID {get; set; }
 
     public int CategoryID {get; set; }
-    public Category Category {get; set; }
+    public Category? Category {get; set; }
     public int Amount {get; set;}
     [Column(TypeName = "nvarchar(75)")]
     public string? Note {get; set;}
     public DateTime Date {get; set;} = DateTime.Now;
 
+    [NotMapped]
+    public string? CategoryTitleWithIcon
+    {
+        get
+        {
+            return Category == null ? "" : Category.Icon + " " + Category.Title;
+        }
+    }
+
+    [NotMapped]
+    public string? FormattedAmount
+    {
+        get
+        {
+            return ((Category == null || Category.Type=="Expense")? "- " :"+ ") + Amount.ToString("C0");
+        }
+    }
+    
 }
