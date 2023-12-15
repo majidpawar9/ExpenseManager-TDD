f453738bea99c20d695063b8b0ca2fdab3dcda1f
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-14 20:30:45 -0500

    Added basic Graph in HomeController to test this feature

--- ExpenseManager-TDD/Controllers/HomeController.cs
+++ ExpenseManager-TDD/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ExpenseManager_TDD.Models;
+using Microsoft.VisualBasic;
 
 namespace ExpenseManager_TDD.Controllers;
 
 public class HomeController : Controller
 {
- 
-    
+
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -23,4 +30,22 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult DonutGraph()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public List<object> GetExpenseData()
+    {
+        List<object> data = new List<object>();
+
+        List<int> labels = _context.Transactions.Select(p =>p.Amount).ToList();
+        data.Add(labels);
+        List<DateTime> date = _context.Transactions.Select(p => p.Date).ToList();
+        data.Add(date);
+        return data;
+    }
+        
 }
